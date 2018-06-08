a11755bea976b09b4ecfddbace0b90a47103693b
Vaibhav Agarwal <vaibhav.sr@gmail.com>
2018-06-08 15:05:54 +0530

    scandata: Update contents to implement scan images
    Helper API reads image files to a numpy array for a given directory and
    file format.
    
    Signed-off-by: Vaibhav Agarwal <vaibhav.sr@gmail.com>

--- scandata.py
+++ scandata.py
@@ -1,4 +1,39 @@
 #!usr/bin/python
-import pandas as pd
+import glob
+import argparse as ap
+import matplotlib.image as img
+import numpy as np
 
+#from PIL import Image
 
+def getimages(fdir = "../dataset/PetImages/Cat", fformat = '.jpg'):
+
+    filelist = glob.glob(fdir+'/*'+fformat)
+    #imgarray = np.array([np.array(Image.open(fname)) for fname in filelist])
+    imgarray = np.array([np.array(img.imread(fname)) for fname in filelist])
+
+    return imgarray
+
+if __name__ == '__main__':
+
+    """ Capture below inputs from the user and initiate imagescan
+    """
+    parser = ap.ArgumentParser()
+    parser.add_argument("-d", "--dir", type=str, help="dir to pick images from", default="../dataset/PetImages/Cat")
+    parser.add_argument("-f", "--format", type=str, help="format of images", default=".jpg")
+    #parser.add_argument("-w", "--write", type=str, help="File name to dump array to")
+
+    #parse the arguments received
+    args = parser.parse_args()
+    fdir = args.dir
+    fformat = args.format
+    #writeto = args.write
+
+    #get images
+    imgarray = getimages(fdir=fdir, fformat=fformat)
+
+    """
+    uncomment following line to dump output array to a file
+    if writeto:
+        imgarray.dump(writeto)
+    """
